ffdc38f216e92eb1c8f1ac66e9030d7305f3fd02
HappyAreaBean <bean@happyareabean.cc>
2024-08-07 03:17:46 +0800

    feat: s3 inital commit (credit: vtardia)

--- config/files.js
+++ config/files.js
@@ -7,5 +7,11 @@ module.exports.files = {
   // Defaults to 500MB
   maxBytes: 524288000,
   // The fs directory name at which files will be kept
-  dirname: ''
+  adapter: require('skipper-s3'),
+  key: process.env.S3_ACCESS_KEY,
+  secret: process.env.S3_SECRET_KEY,
+  bucket: process.env.S3_BUCKET,
+  region: process.env.S3_REGION || undefined,
+  endpoint: process.env.S3_ENDPOINT || undefined,
+  token: process.env.S3_TOKEN || undefined
 };
